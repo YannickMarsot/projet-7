5f726d769c8a26ed2993e36fe884f563c7016545
Yannick Marsot <yannmarsot@hotmail.fr>
2022-10-12 16:21:00 +0200

    Ajout de la page d'erreur en cas d'URL non valide

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
+import Error from './Components/Error'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
@@ -9,6 +10,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="*" element={<Error />}></Route>
     </Routes>
   </BrowserRouter>
 )
