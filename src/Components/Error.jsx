f959f169ab6aeaa0621e1d3919bd664275d94cb8
Yannick Marsot <yannmarsot@hotmail.fr>
2022-11-03 15:08:58 +0100

    Changement page error

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,7 +1,8 @@
 function Error() {
   return (
     <div>
-      <h1>Oups! Cette page n'existe pas</h1>
+      <h1>404</h1>
+      <h2>Oups! Cette page que vous demandez n'existe pas.</h2>
     </div>
   )
 }
