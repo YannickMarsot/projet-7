f095b1857eca6c42b8058de151643a6f67eb760f
Yannick Marsot <yannmarsot@hotmail.fr>
2022-11-04 17:47:35 +0100

    ajout style pour le composant header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,21 @@
 import { Link } from 'react-router-dom'
+import Logo from '../Assets/logo/LOGO.png'
+import '../Utils/Style/Header.css'
 
 function Header() {
   return (
-    <div>
-      <Link to="/">Accueil</Link>
-      <Link to="/AboutUs">A propos</Link>
+    <div className="headerWrapper">
+      <div className="logoWrapper">
+        <img src={Logo} alt="Kasa" id="logo" />
+      </div>
+      <div className="linksWrapper">
+        <Link to="/">
+          <p id="home">Accueil</p>
+        </Link>
+        <Link to="/AboutUs">
+          <p id="aboutUs">A propos</p>
+        </Link>
+      </div>
     </div>
   )
 }
