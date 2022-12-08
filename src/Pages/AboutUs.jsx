d5246aec7c17a2079245e3a82aa9b89baed11e92
Yannick Marsot <yannmarsot@hotmail.fr>
2022-12-08 22:28:26 +0100

    Correction text

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -14,7 +14,7 @@ function AboutUs() {
             <h2 className="AboutUsSubTitle">Fiabilité</h2>
           </div>
           <div className="rectangle3">
-            <p>
+            <p className="text">
               Les annonces postées sur Kasa garantissent une fiabilité totale.
               Les photos sont conformes aux logements, et toutes les
               informations sont régulièrement vérifiées par nos équipes.
@@ -26,7 +26,7 @@ function AboutUs() {
             <h2 className="AboutUsSubTitle">Respect</h2>
           </div>
           <div className="rectangle3">
-            <p>
+            <p className="text">
               La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
               comportement discriminatoire ou de perturbation du voisinage
               entraînera une exclusion de notre plateforme.
@@ -38,7 +38,7 @@ function AboutUs() {
             <h2 className="AboutUsSubTitle">Service</h2>
           </div>
           <div className="rectangle3">
-            <p>
+            <p className="text">
               Nos équipes se tiennent à votre disposition pour vous fournir une
               expérience parfaite. N'hésitez pas à nous contacter si vous avez
               la moindre question.
@@ -50,7 +50,7 @@ function AboutUs() {
             <h2 className="AboutUsSubTitle">Securité</h2>
           </div>
           <div className="rectangle3">
-            <p>
+            <p className="text">
               La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
               pour les voyageurs, chaque logement correspond aux critères de
               sécurité établis par nos services. En laissant une note aussi bien
