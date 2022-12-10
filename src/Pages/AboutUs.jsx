e8a7857a259e5882bd4ab798426872e2f04dc0e8
Yannick Marsot <yannmarsot@hotmail.fr>
2022-12-10 15:43:18 +0100

    Correction style et nommage classe AboutUs

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,16 +4,16 @@ import '../Utils/Style/aboutUs.css'
 
 function AboutUs() {
   return (
-    <div id="contentsWrapper">
+    <div id="bigWrapper">
       <div id="bannerWrapper">
         <img src={banner} alt="baniére" id="banner" />
       </div>
       <div id="textWrapper">
-        <div className="rectangle1">
-          <div className="rectangle2">
+        <div className="contentsWrapper">
+          <div className="subTitleWrapper">
             <h2 className="AboutUsSubTitle">Fiabilité</h2>
           </div>
-          <div className="rectangle3">
+          <div className="TextWrapper">
             <p className="text">
               Les annonces postées sur Kasa garantissent une fiabilité totale.
               Les photos sont conformes aux logements, et toutes les
@@ -21,11 +21,11 @@ function AboutUs() {
             </p>
           </div>
         </div>
-        <div className="rectangle1">
-          <div className="rectangle2">
+        <div className="contentsWrapper">
+          <div className="subTitleWrapper">
             <h2 className="AboutUsSubTitle">Respect</h2>
           </div>
-          <div className="rectangle3">
+          <div className="TextWrapper">
             <p className="text">
               La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
               comportement discriminatoire ou de perturbation du voisinage
@@ -33,11 +33,11 @@ function AboutUs() {
             </p>
           </div>
         </div>
-        <div className="rectangle1">
-          <div className="rectangle2">
+        <div className="contentsWrapper">
+          <div className="subTitleWrapper">
             <h2 className="AboutUsSubTitle">Service</h2>
           </div>
-          <div className="rectangle3">
+          <div className="TextWrapper">
             <p className="text">
               Nos équipes se tiennent à votre disposition pour vous fournir une
               expérience parfaite. N'hésitez pas à nous contacter si vous avez
@@ -45,11 +45,11 @@ function AboutUs() {
             </p>
           </div>
         </div>
-        <div className="rectangle1">
-          <div className="rectangle2">
+        <div className="contentsWrapper">
+          <div className="subTitleWrapper">
             <h2 className="AboutUsSubTitle">Securité</h2>
           </div>
-          <div className="rectangle3">
+          <div className="TextWrapper">
             <p className="text">
               La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
               pour les voyageurs, chaque logement correspond aux critères de
