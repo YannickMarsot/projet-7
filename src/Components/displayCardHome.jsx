91b8da3dc39a04be4430a6ca0e396bb361c3eb89
Yannick Marsot <yannmarsot@hotmail.fr>
2022-12-12 19:38:34 +0100

    correction media queries + debut debuggage dynamisation

--- src/Components/displayCardHome.jsx
+++ src/Components/displayCardHome.jsx
@@ -1,14 +1,14 @@
-import CardHome from '../Components/CardHome'
+// import CardHome from '../Components/CardHome'
 import listAppart from '../Data/listAppart.json'
 
 function DisplayCardHome() {
   listAppart.forEach((element) => {
     return (
-      <div className="cardHomeContainer">
-        <CardHome title={element.title} cover={element.cover} id={element.id} />
-      </div>
-      //console.log(element.id)
-      //<p>coucou</p>
+      // <div className="cardHomeContainer">
+      //   <CardHome title={element.title} cover={element.cover} id={element.id} />
+      // </div>
+      console.log(element)
+      // <p>coucou</p>
     )
   })
 }
