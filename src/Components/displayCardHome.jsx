46fe9e99036b78421779e084044e168af70a7bb5
Yannick Marsot <yannmarsot@hotmail.fr>
2022-12-21 17:06:04 +0100

    displayCarHome finalisation

--- src/Components/displayCardHome.jsx
+++ src/Components/displayCardHome.jsx
@@ -1,21 +1,32 @@
-import CardHome from '../Components/CardHome'
-import listAppart from '../Data/listAppart.json'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-function DisplayCardHome() {
+const DisplayCardHome = (props) => {
   return (
     <div className="cardHomeContainer">
-      {listAppart.map((data, index) => [
-        <CardHome
-          key={data.id + index}
-          title={data.title}
-          cover={data.cover}
-          id={data.id}
-        />,
-        console.log(data.id),
-        <p>coucou</p>,
-      ])}
+      {props.data.map((content) => (
+        <Link to={`Page/${content.id}`}>
+          <div className="CardHome">
+            <h2 className="CardTitle">{content.title}</h2>
+            <img src={content.cover} alt="cover" className="imgHome" />
+          </div>
+        </Link>
+      ))}
     </div>
   )
+}
+
+DisplayCardHome.propTypes = {
+  title: PropTypes.string.isRequired,
+  cover: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+}
+
+DisplayCardHome.defaultProps = {
+  title: 'titre par default',
+  cover: 'C:/Users/Yannm/Desktop/projet-7/src/Assets/Banner/IMG.png',
+  id: '*',
+  //mettre "DefaultCover"
 }
 
 export default DisplayCardHome
