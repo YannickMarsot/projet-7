ca4ff8eae5c788a41dce35d3956932067fe1125c
Yannick Marsot <yannmarsot@hotmail.fr>
2022-12-21 13:20:43 +0100

    Dynamisation Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import DisplayCardHome from '../Components/displayCardHome'
+import listAppart from '../Data/listAppart.json'
 // import CardHome from '../Components/CardHome'
 import IMG from '../Assets/Banner/IMG.png'
 import '../Utils/Style/Style.css'
@@ -12,112 +13,10 @@ function Home() {
         <img src={IMG} alt="Banner" id="IMG" />
       </div>
       <div id="ContentWrapper">
-        <DisplayCardHome />
+        <DisplayCardHome data={listAppart} />
       </div>
     </div>
   )
 }
-
-// function Home() {
-//   return (
-//     <div id="BigWrapper">
-//       <div id="BannerWrapper">
-//         <img src={IMG} alt="Banner" id="IMG" />
-//       </div>
-//       <div id="ContentWrapper">
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//         <div className="CardHome">
-//           <h2 className="CardTitle">Appartement cosy</h2>
-//           <img
-//             src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
-//             alt="cover"
-//             className="imgHome"
-//           />
-//           {/* id="c67ab8a7" */}
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
 
 export default Home
