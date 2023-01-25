819297517be15d56579bea439949065bc152dc61
Yannick Marsot <yannmarsot@hotmail.fr>
2023-01-25 12:19:52 +0100

    mise en place useState, useEffect dans page

--- src/Pages/Page.jsx
+++ src/Pages/Page.jsx
@@ -1,25 +1,27 @@
-/* eslint-disable no-lone-blocks */
+import { useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import listAppart from '../Data/listAppart.json'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import '../Utils/Style/PageTempo.css'
-import { useParams, useNavigate, Navigate } from 'react-router-dom'
-import listAppart from '../Data/listAppart.json'
 
 function Page() {
   const params = useParams()
   const navigate = useNavigate()
-  let id = params.id
-  // console.log(id)
-  {
-    listAppart.map((content) => {
-      if (content.id === id) {
-        console.log(content.id)
-        return null
-      } else {
-        navigate('/*')
-        //envoie l'utilisateur à la page d'erreur
+  const [chosenAppart, setChosenAppart] = useState()
+  useEffect(() => {
+    const getAppart = async () => {
+      const chosenOne = listAppart.find(({ id }) => id === params.id)
+      //on récupére l'appart correspondant à l'id en parametre URL
+      //console.log(chosenOne)
+      listAppart.map(() => setChosenAppart(chosenOne))
+      if (chosenOne === undefined) {
+        navigate('/*') //envoie l'utilisateur à la page d'erreur si l'appart ne correspond pas au parametre URL
       }
-    })
-  }
+    }
+    getAppart()
+    // eslint-disable-next-line
+  }, []) //tableau vide pour lancer useEffect qu'une fois
+  console.log(chosenAppart)
 }
 
 export default Page
