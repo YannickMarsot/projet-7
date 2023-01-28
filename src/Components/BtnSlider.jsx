f977e914f53a374848e16ebeda1a5daf1e2099de
Yannick Marsot <yannmarsot@hotmail.fr>
2023-01-28 18:22:57 +0100

    Ajout components Slider

---
+++ src/Components/BtnSlider.jsx
@@ -0,0 +1,18 @@
+import leftArrow from '../Assets/Arrows/left.png'
+import rightArrow from '../Assets/Arrows/right.png'
+
+function BtnSlider({ direction, moveSlide }) {
+  return (
+    <button
+      onClick={moveSlide}
+      className={direction === 'next' ? 'btn-slide next' : 'btn-slide prev'}
+    >
+      <img
+        src={direction === 'next' ? rightArrow : leftArrow}
+        alt="btn-arrow"
+      />
+    </button>
+  )
+}
+
+export default BtnSlider
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,7 +1,49 @@
+import { useState } from 'react'
+import BtnSlider from './BtnSlider'
+import '../Utils/Style/Slider.css'
+
 function Slider(props) {
-  return props.data.map((content) => {
-    return <img src={content} alt="SliderImg" />
-  })
+  //const length = props.data.length // longueur du tableau de slides
+  const [slideIndex, setSlideIndex] = useState(1)
+
+  const nextSlide = () => {
+    if (slideIndex !== props.data.length) {
+      setSlideIndex(slideIndex + 1)
+    } else if (slideIndex === props.data.length) {
+      setSlideIndex(1)
+    }
+  }
+
+  const prevSlide = () => {
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1)
+    } else if (slideIndex === 1) {
+      setSlideIndex(props.data.length)
+    }
+  }
+  return (
+    <div id="container-slider">
+      {props.data.map((content, index) => {
+        return (
+          <div>
+            <div
+              key={index}
+              className={
+                slideIndex === index + 1 ? 'slide active-anim' : 'slide'
+              }
+            >
+              <img src={content} alt="SliderImg" />
+            </div>
+            <BtnSlider moveSlide={nextSlide} direction={'next'} />
+            <BtnSlider moveSlide={prevSlide} direction={'prev'} />
+            {/* <span className="slider__number">
+              {index + 1}/{length}
+            </span> */}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Slider
