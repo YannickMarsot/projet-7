2ac3b9b15fd8156496b28fffa9ffef8465af69fd
Yannick Marsot <yannmarsot@hotmail.fr>
2023-01-30 15:44:15 +0100

    Ajout components Tags

---
+++ src/Components/Tags.jsx
@@ -0,0 +1,13 @@
+import '../Utils/Style/Tags.css'
+
+function Tags(props) {
+  return props.data.map((content, index) => {
+    return (
+      <li className="tagsWrapper" key={index}>
+        <p className="tagsText">{content}</p>
+      </li>
+    )
+  })
+}
+
+export default Tags
