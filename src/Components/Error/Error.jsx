e5175eebecb8e945570150603a7099ec7f357084
Yannick Marsot <yannmarsot@hotmail.fr>
2023-01-31 13:57:48 +0100

    ajout bouton retour accueil page error

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import '../Error/Error.css'
 
 function Error() {
@@ -5,6 +6,9 @@ function Error() {
     <div className="elementsWrapper">
       <h1 id="ErrorTitle">404</h1>
       <h2 id="ErrorText">Oups! Cette page que vous demandez n'existe pas.</h2>
+      <Link to="/">
+        <p className="LinkError">Retourner sur la page d'Accueil</p>
+      </Link>
     </div>
   )
 }
