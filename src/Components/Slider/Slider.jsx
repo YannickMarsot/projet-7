8982c014a5c485a151bf92cc2dd4399add00f84e
Yannick Marsot <yannmarsot@hotmail.fr>
2023-02-10 11:38:37 +0100

    Correction Slider 1 img

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -3,13 +3,13 @@ import BtnSlider from '../BtnSlider/BtnSlider'
 import '../Slider/Slider.css'
 
 function Slider(props) {
-  //const length = props.data.length // longueur du tableau de slides
+  const length = props.data.length // longueur du tableau de slides
   const [slideIndex, setSlideIndex] = useState(1)
 
   const nextSlide = () => {
-    if (slideIndex !== props.data.length) {
+    if (slideIndex !== length) {
       setSlideIndex(slideIndex + 1)
-    } else if (slideIndex === props.data.length) {
+    } else if (slideIndex === length) {
       setSlideIndex(1)
     }
   }
@@ -18,7 +18,7 @@ function Slider(props) {
     if (slideIndex !== 1) {
       setSlideIndex(slideIndex - 1)
     } else if (slideIndex === 1) {
-      setSlideIndex(props.data.length)
+      setSlideIndex(length)
     }
   }
   return (
@@ -26,22 +26,39 @@ function Slider(props) {
       {props.data.map((content, index) => {
         //console.log(content)
         //mettre if else pour affichage bouton si sup à  1 retourner ci dessous sinon retourner sans boutton
-        return (
-          <div key={index}>
-            <div
-              className={
-                slideIndex === index + 1 ? 'slide active-anim' : 'slide'
-              }
-            >
-              <img src={content} alt="SliderImg" />
-            </div>
-            <BtnSlider moveSlide={nextSlide} direction={'next'} />
-            <BtnSlider moveSlide={prevSlide} direction={'prev'} />
-            {/* <span className="slider__number">
+        if (length > 1) {
+          return (
+            <div key={index}>
+              <div
+                className={
+                  slideIndex === index + 1 ? 'slide active-anim' : 'slide'
+                }
+              >
+                <img src={content} alt="SliderImg" />
+              </div>
+              <BtnSlider moveSlide={nextSlide} direction={'next'} />
+              <BtnSlider moveSlide={prevSlide} direction={'prev'} />
+              {/* <span className="slider__number">
               {index + 1}/{length}
             </span> */}
-          </div>
-        )
+            </div>
+          )
+        } else {
+          return (
+            <div key={index}>
+              <div
+                className={
+                  slideIndex === index + 1 ? 'slide active-anim' : 'slide'
+                }
+              >
+                <img src={content} alt="SliderImg" />
+              </div>
+              {/* <span className="slider__number">
+                {index + 1}/{length}
+              </span> */}
+            </div>
+          )
+        }
       })}
     </div>
   )
