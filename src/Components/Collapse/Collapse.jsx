841a844c64f7d5f8450e79b33f27b8c4c04dced8
Yannick Marsot <yannmarsot@hotmail.fr>
2023-02-10 11:32:57 +0100

    ajout flèche collapse

--- src/Components/Collapse/Collapse.jsx
+++ src/Components/Collapse/Collapse.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import arrow from '../../Assets/Arrows/down.png'
 import '../Collapse/Collapse.css'
 
 const Collapsible = (props) => {
@@ -12,6 +13,9 @@ const Collapsible = (props) => {
       <div className="subTitleWrapper">
         <h2 className="subTitle" onClick={toggle}>
           {props.label}
+          <span className={open ? 'arrow_reverse' : 'arrow'}>
+            <img src={arrow} alt="arrow collapse" />
+          </span>
         </h2>
       </div>
       <div>{open && <div className="text">{props.children}</div>}</div>
